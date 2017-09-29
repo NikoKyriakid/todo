7046c2f09edae55ce43464461dc73111e32b434a
Niko Kyriakidis <nick.kyriakid@gmail.com>
2017-09-29 17:15:01 +0100

    initial commit2

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!')
+// })
+
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
+
