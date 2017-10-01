228124677880cfc4de0aedfb0859d93739bc27e9
Niko Kyriakidis <nick.kyriakid@gmail.com>
2017-10-01 20:25:48 +0100

    server not responding

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
 app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+    console.log('Nick server is starting.....')
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
 module.exports = app;
