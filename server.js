a2fb86d442c945cf80af30ab9c4cd50138d50af0
NikoKyriakid <knikos21@gmail.com>
2018-03-12 13:25:14 -0400

    restart the project.

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    console.log(req)
+  console.log(process.env);
     res.send('Hello World!')
 
-})
-
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
+});
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
